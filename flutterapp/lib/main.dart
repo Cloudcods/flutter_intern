36a5527bb374aefe8c7aeefdaabe3afb65f0ba5a
Suman Ghimire <ghimiresuman116@gmail.com>
2024-07-25 12:09:38 +0545

    day1 completed

---
+++ flutterapp/lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutterapp/home-page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 64, 37, 110)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
